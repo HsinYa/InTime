d9b5a94be062b9d397f8dbc98f245986862ac68e
HsinYa <starrysky012499@gmail.com>
2016-12-10 12:59:45 +0800

    Add feature login with ID and title
    Connect the realtime databasse of firebase

--- app/src/main/java/com/example/cindy/intime/MainActivity.java
+++ app/src/main/java/com/example/cindy/intime/MainActivity.java
@@ -5,9 +5,12 @@ import android.content.DialogInterface;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -18,12 +21,21 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
     FirebaseAuth auth;
     FirebaseAuth.AuthStateListener authListener;
     private String userUID;
 
+    private RadioGroup rgroup;
+    private RadioButton rstudent;
+    private RadioButton rteacher;
+    private String title; //student or teacher
+
+    private DatabaseReference mFirebaseDatabase;
+    private FirebaseDatabase mFirebaseInstance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +60,12 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
+        mFirebaseInstance = FirebaseDatabase.getInstance();
+        // get reference to 'users' node
+        mFirebaseDatabase = mFirebaseInstance.getReference("users");
 
+        // store app title to 'app_title' node
+        mFirebaseInstance.getReference("app_title").setValue("Realtime Database");
     }
 
     //when MainActivity  first display or run from background
@@ -65,28 +82,53 @@ public class MainActivity extends AppCompatActivity {
         auth.removeAuthStateListener(authListener);
     }
 
-    //Login with email & password
+    //Login with id ,email,password and title
     public void login(View v){
+        final String id = ((EditText)findViewById(R.id.idName))
+                .getText().toString();
         final String email = ((EditText)findViewById(R.id.email))
                 .getText().toString();
         final String password = ((EditText)findViewById(R.id.password))
                 .getText().toString();
         Log.d("AUTH", email+"/"+password);
+
+        //RadioGroup checked and handle
+        rgroup = (RadioGroup)findViewById(R.id.rgroup);
+        rstudent = (RadioButton)findViewById(R.id.radioStudent);
+        rteacher = (RadioButton)findViewById(R.id.radioTeacher);
+        rgroup.setOnCheckedChangeListener(listener);
+
         auth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (!task.isSuccessful()){
                             Log.d("onComplete", "登入失敗");
-                            //create a new account of user
-                            register(email, password);
+                            register(id,email, password,title);
                         }
                     }
                 });
     }
 
+    //RadioGroup checked and handle the title of teacher or student
+    private RadioGroup.OnCheckedChangeListener listener = new
+            RadioGroup.OnCheckedChangeListener(){
+                @Override
+                public void onCheckedChanged(RadioGroup group,int checkedId){
+                    int p = group.indexOfChild((RadioButton)findViewById(checkedId));
+                    switch (checkedId){
+                        case R.id.radioStudent:
+                            title = "student";
+                            break;
+                        case R.id.radioTeacher:
+                            title = "teacher";
+                            break;
+                    }
+                }
+            };
+
     //register a new user
-    private void register(final String email, final String password) {
+    private void register(final String id,final String email, final String password,final String title) {
         new AlertDialog.Builder(MainActivity.this)
                 .setTitle("登入問題")
                 .setMessage("無此帳號，是否要以此帳號與密碼註冊?")
@@ -94,14 +136,17 @@ public class MainActivity extends AppCompatActivity {
                         new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-                                createUser(email, password);
+                                createUser(id,email, password,title);
                             }
                         })
                 .setNeutralButton("取消", null)
                 .show();
     }
 
-    private void createUser(String email, String password) {
+    /**
+     * Creating new user node under 'users'
+     */
+    private void createUser(String id,String email, String password,String title) {
         auth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(
                         new OnCompleteListener<AuthResult>() {
@@ -116,5 +161,19 @@ public class MainActivity extends AppCompatActivity {
                                         .show();
                             }
                         });
+
+        //this userId should be fetched by implementing firebase auth
+        // Creating new user node, which returns the unique key value
+        // new user node would be /users/$userid/
+        if(TextUtils.isEmpty(userUID)){
+            userUID = mFirebaseDatabase.push().getKey();
+        }
+
+        User user = new User(id,email,password,title);
+        //get the reference to ‘users’ node using child() method
+        //use setValue() method to store the user data
+        mFirebaseDatabase.child(userUID).setValue(user);
+
     }
+
 }
---
+++ app/src/main/java/com/example/cindy/intime/User.java
@@ -0,0 +1,25 @@
+package com.example.cindy.intime;
+
+/**
+ * Created by cindy on 2016/12/10.
+ */
+
+public class User {
+    //information of user account
+    public String name; //id
+    public String email;
+    public String password;
+    public String title; //student or teacher
+
+    // Default constructor required for calls to DataSnapshot.getValue(User.class)
+    public User(){
+
+    }
+
+    public User(String name,String email,String password,String title){
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.title = title;
+    }
+}
