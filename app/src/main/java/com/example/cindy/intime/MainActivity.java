db55a3d636f7e4f388c04929f41ed4002dfdff63
HsinYa <starrysky012499@gmail.com>
2016-12-10 11:36:56 +0800

    Add login by email & password
    Connect with Authentication of Firebase

--- app/src/main/java/com/example/cindy/intime/MainActivity.java
+++ app/src/main/java/com/example/cindy/intime/MainActivity.java
@@ -1,13 +1,70 @@
 package com.example.cindy.intime;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class MainActivity extends AppCompatActivity {
+    FirebaseAuth auth;
+    FirebaseAuth.AuthStateListener authListener;
+    private String userUID;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Get the object of  FirebaseAuth
+        auth = FirebaseAuth.getInstance();
+        //Judge the state of login or logout
+        authListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(
+                    @NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user!=null) {
+                    Log.d("onAuthStateChanged", "登入:"+
+                            user.getUid());
+                    userUID =  user.getUid();
+                }else{
+                    Log.d("onAuthStateChanged", "已登出");
+                }
+            }
+        };
+
+
     }
+
+    //when MainActivity  first display or run from background
+    @Override
+    protected void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(authListener);
+    }
+
+    //when MainActivity stop or entry background
+    @Override
+    protected void onStop() {
+        super.onStop();
+        auth.removeAuthStateListener(authListener);
+    }
+
+    //Login with email & password
+    public void login(View v){
+        final String email = ((EditText)findViewById(R.id.email))
+                .getText().toString();
+        final String password = ((EditText)findViewById(R.id.password))
+                .getText().toString();
+        Log.d("AUTH", email+"/"+password);
+        auth.signInWithEmailAndPassword(email, password);
+    }
+
 }
