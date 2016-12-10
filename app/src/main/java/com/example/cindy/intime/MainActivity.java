7f02d0bf2584b0c0e420ef5d4594fc57f667a912
HsinYa <starrysky012499@gmail.com>
2016-12-10 12:16:48 +0800

    Add feature of register a new account
    create a new user account in firebase

--- app/src/main/java/com/example/cindy/intime/MainActivity.java
+++ app/src/main/java/com/example/cindy/intime/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.cindy.intime;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,9 +9,15 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 
 public class MainActivity extends AppCompatActivity {
     FirebaseAuth auth;
@@ -64,7 +72,49 @@ public class MainActivity extends AppCompatActivity {
         final String password = ((EditText)findViewById(R.id.password))
                 .getText().toString();
         Log.d("AUTH", email+"/"+password);
-        auth.signInWithEmailAndPassword(email, password);
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()){
+                            Log.d("onComplete", "登入失敗");
+                            //create a new account of user
+                            register(email, password);
+                        }
+                    }
+                });
     }
 
+    //register a new user
+    private void register(final String email, final String password) {
+        new AlertDialog.Builder(MainActivity.this)
+                .setTitle("登入問題")
+                .setMessage("無此帳號，是否要以此帳號與密碼註冊?")
+                .setPositiveButton("註冊",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                createUser(email, password);
+                            }
+                        })
+                .setNeutralButton("取消", null)
+                .show();
+    }
+
+    private void createUser(String email, String password) {
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(
+                        new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                String message =
+                                        task.isSuccessful() ? "註冊成功" : "註冊失敗";
+
+                                new AlertDialog.Builder(MainActivity.this)
+                                        .setMessage(message)
+                                        .setPositiveButton("OK", null)
+                                        .show();
+                            }
+                        });
+    }
 }
